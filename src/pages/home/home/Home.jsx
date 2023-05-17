0ee9cec3cd2c9007dbee47c0add809fea0caba95
jahid morol <jahidmorol2@gmail.com>
2023-05-18 01:44:19 +0600

    implant banner with responsive

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import backgroundImage from '../../../assets/hero-bg.png'
+import React from "react";
+import backgroundImage from "../../../assets/hero-bg.png";
+import child from "../../../assets/slider-img.png";
 
 const Home = () => {
-    // const backgroundImage = 'url("../../../assets/hero-bg.png")'
-    return (
-        <div className='h-[100vh]' style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover',backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
-            its home component
+  // const backgroundImage = 'url("../../../assets/hero-bg.png")'
+  return (
+    <div className="">
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          // backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+        className="hero min-h-screen bg-base-200"
+      >
+        <div className="hero-content flex-col lg:flex-row-reverse gap-5 md:gap-10">
+          <img src={child} className=" rounded-lg w-[90%] md:w-[45%]" />
+          <div className="text-white max-w-lg">
+            <h1 className="text-4xl md:text-5xl font-bold">
+              BEST TOYS FOR <br /> YOUR KID!
+            </h1>
+            <p className="py-6">
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+            <button className="btn  bg-[#203247]">Get Started</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
