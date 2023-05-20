31416d85b9922a36209d470326a0c4f67f3ddfb8
jahid morol <jahidmorol2@gmail.com>
2023-05-20 11:03:01 +0600

    implant serch bar in allToys page

--- src/pages/allToys/Toys.jsx
+++ src/pages/allToys/Toys.jsx
@@ -3,34 +3,66 @@ import { useState } from "react";
 import Toy from "./Toy";
 
 const Toys = () => {
-    const [toys, setToys] = useState([])
+  const [toys, setToys] = useState([]);
+  const [seacrhToy, setSearchToy] = useState('')
+  console.log(seacrhToy);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/alltoys')
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data);
-            setToys(data)
-        })
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:5000/alltoys")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setToys(data);
+      });
+  }, []);
+  
+  const handleSerchBtn = () => {
+    fetch(`http://localhost:5000/searchbyname/${seacrhToy}`,)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setToys(data)
+    })
+  }
 
   return (
-    <div className="bg-[#3DC2CA]">
-        <div className="my-container">
-      <div className="text-center">
-        <h2 className="font-bold text-4xl mb-5">New Arrivals</h2>
+    <div className="">
+      <div className="my-container">
+        <div className="text-center">
+          <h2 className="font-bold text-4xl mb-5">New Arrivals</h2>
+        </div>
+        <div className="w-[77%] md:w-[22%] my-5 mx-auto">
+          <div className="input-group ">
+            <input
+              type="text"
+              onChange={(e) => setSearchToy(e.target.value)}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSerchBtn} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="grid gap-5 md:gap-10 md:grid-cols-2 lg:grid-cols-3">
+          {toys.map((toy) => (
+            <Toy key={toy._id} toy={toy}></Toy>
+          ))}
+        </div>
       </div>
-      <div className="grid gap-5 md:gap-10 md:grid-cols-2 lg:grid-cols-3">
-        {
-            toys.map(toy => <Toy key={toy._id} toy={toy}></Toy>)
-        }
-      </div>
-      <div className="w-[10rem] mx-auto my-5 md:my-10">
-        <button className="btn btn-outline hover:bg-[#FF3811] border-[#FF3811]">
-          More Services
-        </button>
-      </div>
-    </div>
     </div>
   );
 };
--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,71 +1,98 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div className="bg-gray-100 min-h-screen ">
+  return (
+    <div className="bg-gray-100 min-h-screen ">
       <div className="my-container py-8">
-        <h1 className="text-3xl font-bold mb-10 text-center">Welcome to My Blog</h1>
+        <div className="relative md:w-[35%] mx-auto">
+          <div className="h-16 absolute right-4 top-2/3 md:top-1/4 mask mask-hexagon-2 bg-[#3DC2CA]">jahid</div>
+          <div className="h-16 absolute left-4 bottom-1/2 md:bottom-1/4 mask mask-hexagon-2 bg-[#3DC2CA]">jahid</div>
+
+          <h1 className=" text-3xl font-bold mb-10 text-center">
+            Welcome to My Blog
+          </h1>
+        </div>
 
         <div className="my-6">
-          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">1. What is an access token and refresh token?</h2>
-          <p className='font-bold text-gray-500 ms-3'># Access Token</p>
+          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">
+            1. What is an access token and refresh token?
+          </h2>
+          <p className="font-bold text-gray-500 ms-3"># Access Token</p>
           <p className="text-gray-800 ms-3">
-            An access token is a credential used to access protected resources on a server. It is typically obtained
-            after successful authentication and is used to authorize subsequent requests.
+            An access token is a credential used to access protected resources
+            on a server. It is typically obtained after successful
+            authentication and is used to authorize subsequent requests.
             <br />
-            <p className='font-bold  mt-3 text-gray-500'># Refresh Token</p>
-            A refresh token is a long-lived token used to obtain a new access token when the current one expires.
-            It is usually stored securely on the client-side, either in an HTTP-only cookie or local storage.
+            <p className="font-bold  mt-3 text-gray-500"># Refresh Token</p>A
+            refresh token is a long-lived token used to obtain a new access
+            token when the current one expires. It is usually stored securely on
+            the client-side, either in an HTTP-only cookie or local storage.
           </p>
         </div>
 
         <div className="my-6">
-          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">2. Compare SQL and NoSQL databases?</h2>
-          <p className='font-bold text-gray-500 ms-3'># SQL</p>
+          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">
+            2. Compare SQL and NoSQL databases?
+          </h2>
+          <p className="font-bold text-gray-500 ms-3"># SQL</p>
           <p className="text-gray-800 ms-3">
-            SQL databases are based on a structured query language and use a predefined schema to organize data into
-            tables with relationships. They provide strong consistency, ACID properties, and are suitable for complex,
-            structured data.
+            SQL databases are based on a structured query language and use a
+            predefined schema to organize data into tables with relationships.
+            They provide strong consistency, ACID properties, and are suitable
+            for complex, structured data.
             <br />
-            <p className='font-bold text-gray-500 mt-3'># NoSQL</p>
-            NoSQL databases, on the other hand, are non-relational and do not rely on a fixed schema. They offer
-            high scalability, eventual consistency, and are suitable for unstructured or semi-structured data,
-            and scenarios with high read/write loads.
+            <p className="font-bold text-gray-500 mt-3"># NoSQL</p>
+            NoSQL databases, on the other hand, are non-relational and do not
+            rely on a fixed schema. They offer high scalability, eventual
+            consistency, and are suitable for unstructured or semi-structured
+            data, and scenarios with high read/write loads.
           </p>
         </div>
 
         <div className="my-6">
-          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">3. What is Express.js? What is Nest.js?</h2>
-          <p className='font-bold text-gray-500 ms-3'># Node.js</p>
+          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">
+            3. What is Express.js? What is Nest.js?
+          </h2>
+          <p className="font-bold text-gray-500 ms-3"># Node.js</p>
           <p className="text-gray-800 ms-3">
-            Express.js is a minimal and flexible Node.js web application framework. It provides a set of features
-            for building web applications and APIs, handling HTTP requests, routing, middleware, and server-side logic.
+            Express.js is a minimal and flexible Node.js web application
+            framework. It provides a set of features for building web
+            applications and APIs, handling HTTP requests, routing, middleware,
+            and server-side logic.
             <br />
-            <p className='font-bold text-gray-500 mt-3'># Nest.js</p>
-            Nest.js is a progressive Node.js framework built on top of Express.js. It leverages TypeScript and
-            follows a modular architecture pattern. Nest.js provides features like dependency injection, decorators,
-            and declarative programming for building scalable and maintainable server-side applications.
+            <p className="font-bold text-gray-500 mt-3"># Nest.js</p>
+            Nest.js is a progressive Node.js framework built on top of
+            Express.js. It leverages TypeScript and follows a modular
+            architecture pattern. Nest.js provides features like dependency
+            injection, decorators, and declarative programming for building
+            scalable and maintainable server-side applications.
           </p>
         </div>
 
         <div className="my-6">
-          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">4. What is MongoDB aggregate and how does it work?</h2>
+          <h2 className="text-2xl text-[#3DC2CA] font-semibold mb-2">
+            4. What is MongoDB aggregate and how does it work?
+          </h2>
           <p className="text-gray-800 ms-3">
-            MongoDB's aggregate is a powerful method used to perform advanced data aggregation and processing tasks.
-            It works by creating an aggregation pipeline that consists of multiple stages, each processing the data
-            in a specific way.
+            MongoDB's aggregate is a powerful method used to perform advanced
+            data aggregation and processing tasks. It works by creating an
+            aggregation pipeline that consists of multiple stages, each
+            processing the data in a specific way.
             <br />
-            Each stage in the pipeline performs operations such as filtering, grouping, sorting, projecting,
-            joining, and mathematical operations on the data. The result of one stage is passed as input to the
-            next stage.
+            Each stage in the pipeline performs operations such as filtering,
+            grouping, sorting, projecting, joining, and mathematical operations
+            on the data. The result of one stage is passed as input to the next
+            stage.
             <br />
-            With the aggregate framework, you can perform complex queries, aggregations, and transformations directly
-            within the MongoDB database, reducing the need to transfer large amounts of data to the client for processing.
+            With the aggregate framework, you can perform complex queries,
+            aggregations, and transformations directly within the MongoDB
+            database, reducing the need to transfer large amounts of data to the
+            client for processing.
           </p>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Blogs;
